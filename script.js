d9169dd752f3daa150f739b47083a2dbf7693a91
lminchala <73615264+lminchala@users.noreply.github.com>
2020-11-04 17:08:19 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".whack1").click(function(){
+    $(".mole1").hide();
+});
+    
+$(".whack2").click(function(){
+    $(".moleTwo").hide();
+});
+    
+$(".whack3").click(function(){
+    $(".thirdMole").hide();
+});
+    
+$(".reset").click(function(){
+    $("img").show();
+});
